d325075638bdf5779532c856ccb26a99d9589885
Shukurjon <shukurjon11223344@gmail.com>
2026-09-21 14:51:41 +0500

    C dasturim qo'shildi

---
+++ main.c
@@ -0,0 +1,65 @@
+// #include <stdio.h>
+
+// struct mashina{
+
+//     int narx;
+//     int yili;
+// };
+
+// int main(void){
+    
+//     struct mashina mashina1;
+   
+//         mashina1.narx = 450000;
+//         mashina1.yili = 2026;
+       
+//     printf("narxi: %d yili %d\n",mashina1.narx, mashina1.yili);
+
+//     return 0;
+    
+
+// }
+
+
+
+
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+// struct kitob{
+
+//     char kitob_nomi[20];
+//     int kitob_sahifasi;
+//     float kitob_narxi;
+
+
+
+// };
+
+// int main(void){
+
+//     struct kitob kitob1;
+//     struct kitob kitob2;
+   
+    
+
+//     strcpy(kitob1.kitob_nomi,"harry poter");
+//     kitob1.kitob_sahifasi = 300;
+//     kitob1.kitob_narxi = 50000;
+
+//     strcpy(kitob2.kitob_nomi,"uzuklar hukumdori");
+//     kitob2.kitob_sahifasi = 12200;
+//     kitob2.kitob_narxi = 100000;
+
+//     printf("Nomi: %s, Sahifasi %d, Narxi %2f\n", kitob1.kitob_nomi,kitob1.kitob_sahifasi,kitob1.kitob_narxi);
+//     printf("Nomi: %s, Sahifasi %d, Narxi %2f\n", kitob2.kitob_nomi,kitob2.kitob_sahifasi,kitob2.kitob_narxi);
+    
+        
+
+//     return 0;
+    
+    
+
+// }
